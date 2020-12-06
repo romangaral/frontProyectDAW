25b55e548e433ad9333d1a4c4cf5cde001c84c95
Paula <paula.rodriguez.balseiro@gmail.com>
2020-12-06 19:42:01 +0100

    Updates

---
+++ precache-manifest.2adffd1dcb3802af5e3d8c82f653d5ba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15ac874a6d577d8910ff7cd55957aba2",
+    "url": "/frontProyectDAW/index.html"
+  },
+  {
+    "revision": "5e8e6797966c8cda3861",
+    "url": "/frontProyectDAW/static/css/2.3c25ef27.chunk.css"
+  },
+  {
+    "revision": "9df2ebbd1bf68ced4cce",
+    "url": "/frontProyectDAW/static/css/main.a9971e42.chunk.css"
+  },
+  {
+    "revision": "5e8e6797966c8cda3861",
+    "url": "/frontProyectDAW/static/js/2.443be57d.chunk.js"
+  },
+  {
+    "revision": "57af66c67ce7743363190356615b253d",
+    "url": "/frontProyectDAW/static/js/2.443be57d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9df2ebbd1bf68ced4cce",
+    "url": "/frontProyectDAW/static/js/main.d2b67731.chunk.js"
+  },
+  {
+    "revision": "772283e7e93d410003da",
+    "url": "/frontProyectDAW/static/js/runtime-main.ff951de0.js"
+  },
+  {
+    "revision": "48885972103391be19857cbd32302db4",
+    "url": "/frontProyectDAW/static/media/ocio_asturias.48885972.jpg"
+  }
+]);
--- src/config.js
+++
@@ -1,7 +0,0 @@
-
-const config = {
-    baseUrl: process.env.REACT_APP_BASE_URL || 'http://localhost:8090',
-    hostingProjectName: '/frontProyectDAW'
-};
-
-export default config;
